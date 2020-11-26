d12f7dde6dd65f3e5d9e3b79c510a1d1f6277873
Виктор Блумрайтер <ska_vitek@mail.ru>
2020-11-26 22:43:44 +0300

    Created admin area

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Bazar.Domain.Entities;
 
 namespace Bazar.Data
 {
@@ -12,5 +13,7 @@ namespace Bazar.Data
             : base(options)
         {
         }
+
+        public DbSet<CarouselSlide> CarouselSlides { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -62,10 +62,20 @@ namespace Bazar
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "MyArea",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapRazorPages();
+
+             
             });
+
+
         }
     }
 }
